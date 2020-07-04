f989166abc3b4f037072633347a623c0a75ca664
susannaberte <susanna.berte@studenti.unitn.it>
2020-07-04 18:34:30 +0200

    Co-authored-by: RiccardoPrandini <riccardo.prandini@studenti.unitn.it> Co-authored-by: cmarche99 <chiara.marche99@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -30,3 +30,80 @@
   color: #42b983;
 }
 </style>
+
+<!--<template>
+//    <md-app  id="app" md-mode="fixed-last">
+//       <md-app-toolbar class="md-primary">
+//         <div class="md-toolbar-row">
+//          <span class="md-title">Sono bella</span>
+//         </div> 
+//         <div class="md-toolbar-row">
+//             <md-button class="md-icon-button" @click="navOpen = true" v-if="!navOpen">
+//             <md-icon>menu</md-icon>
+//           </md-button>
+//           <span>My Title</span>
+//         </div> 
+//       </md-app-toolbar> 
+      
+//       <md-app-drawer :md-active.sync="navOpen" md-persistent="full">
+        
+//         <md-toolbar class="md-transparent" md-elevation="0">
+//           <span>Navigation</span>
+//           <div class="md-toolbar-section-end">
+//             <md-button class="md-icon-button" @click="navOpen = false">
+//               <md-icon>keyboard_arrow_left</md-icon>
+//             </md-button>
+//           </div>
+//         </md-toolbar>
+
+//         <md-list>
+//           <md-list-item>
+//             <md-icon>move_to_inbox</md-icon>
+//             <span class="md-list-item-text">Inbox</span>
+//           </md-list-item>
+//           <md-list-item>
+//             <md-icon>send</md-icon>
+//             <span class="md-list-item-text">Sent Mail</span>
+//           </md-list-item>
+//           <md-list-item>
+//             <md-icon>delete</md-icon>
+//             <span class="md-list-item-text">Trash</span>
+//           </md-list-item>
+//           <md-list-item>
+//             <md-icon>error</md-icon>
+//             <span class="md-list-item-text">Spam</span>
+//           </md-list-item>
+//           <md-list-item>
+//             <md-icon>delete</md-icon>
+//             <span class="md-list-item-text">Trash</span>
+//           </md-list-item>
+//           <md-list-item>
+//             <md-icon>error</md-icon>
+//             <span class="md-list-item-text">Spam</span>
+//           </md-list-item>
+//         </md-list>
+
+//       </md-app-drawer> 
+      
+//       <md-app-content> 
+//         <router-view></router-view>
+//       </md-app-content> 
+//   </md-app> 
+// </template>
+
+// <script>
+// export default {
+//   name: "App",
+//   data: function(){
+//     return{
+//       navOpen: false
+//     }
+//   }
+// };
+// </script>
+
+// <style>
+// .md-app{
+//   height: 100vh;
+// }
+// </style>-->
--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import VueMaterial from 'vue-material'; 
+
+// import "vue-material/dist/vue-material.min.css"; 
+// import "vue-material/dist/theme/default.css"; 
 
 Vue.config.productionTip = false
+
+Vue.use(VueMaterial); 
+Vue.use(VueRouter);
+
+// const router = new VueRouter({
+//   mode: "history",
+//   routes: [
+//     { path: '/home', component: Home },
+//     { path: './page1', component: Page1 },
+//     { path: '/', redirect: '/home' }
+//   ]
+// });
 
 new Vue({
   router,
