051350f16a3ea167eecf284c2988be32c6cdef40
Chiara <chiara.marche99@gmail.com>
2020-07-08 22:31:53 +0200

    app vue e home ricerca
    abbiamo impostato l'app vue e creato la home con la ricerca delle canzoni

---
+++ src/components/cardCanzone.vue
@@ -0,0 +1,38 @@
+<template id="cardCanzone">
+    <div class="md-size-33 md-layout-item md-small-size-50 md-xsmall-size-100">
+        <md-card>
+            <md-card-header>
+            <md-card-header-text>
+            <div class="md-title">{{canzone.track | truncate(20)}}</div>
+            <div class="md-subhead">{{canzone.artist}}</div>
+            </md-card-header-text>
+
+            <md-card-media>
+                <img :src= canzone.cover>
+            </md-card-media>
+        </md-card-header>
+
+        <md-card-actions>
+            <md-button>Dettagli canzone</md-button>
+        </md-card-actions>
+        </md-card>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["canzone"],
+    filters: {
+        truncate: function(source, value) {
+            return source.length > value ? source.slice(0, value - 1) + "…" : source;
+        }
+    }
+
+}   
+</script>
+
+<style>
+    .md-card {
+        margin: 5%;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,41 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import VueRouter from 'vue-router'
+import VueMaterial from 'vue-material'
+
+import "vue-material/dist/vue-material.min.css"; 
+import "vue-material/dist/theme/default.css"; 
+
+import Home from "./views/Home";
+
+Vue.use(VueMaterial);
+Vue.use(VueRouter);
 
 Vue.config.productionTip = false
+
+const router = new VueRouter({
+  mode: "history",
+  routes: [
+      { path: '/home', component: Home },
+    // { path: './page1', component: Page1 },
+      { path: '/', redirect: '/home' }
+  ]
+});
 
 new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+var filter = function(text, length, clamp){
+  clamp = clamp || '...';
+  var node = document.createElement('div');
+  node.innerHTML = text;
+  var content = node.textContent;
+  return content.length > length ? content.slice(0, length) + clamp : content;
+};
+
+Vue.filter('truncate', filter);
+
+
+
