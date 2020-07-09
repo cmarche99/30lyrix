c2ee694ffe1afc3007002b2edd7077963699c1c5
susannaberte <susanna.berte@studenti.unitn.it>
2020-07-09 09:48:10 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,49 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">riguardo a noi</router-link>
-    </div>
-    <router-view/>
-  </div>
+   <md-app  id="App" md-mode="fixed-last">
+      <md-app-toolbar class="md-primary">
+        <div class="md-toolbar-section start">
+         <md-button>
+         <span class="md-title">30lyrix</span>
+         </md-button>
+        </div> 
+        <div class="md-toolbar-section-end">
+            <md-button class="md-icon-button">
+            <md-icon>favorite</md-icon>
+          </md-button>
+        </div> 
+      </md-app-toolbar> 
+      
+      <md-app-content> 
+        <router-view></router-view>
+      </md-app-content> 
+  </md-app> 
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import axios from 'axios'
+
+
+export default {
+  data: function() {
+      return {
+      prova: 'ciao'
+    }
+  },
+  // created(){
+  //   this.fetch();
+  // },
+  // methods: {
+  //   fetch(){
+  //     axios.get('https://api.happi.dev/v1/music?q=umbrella%20rihanna&limit=&apikey=945335Zmq8pmd4LEAhl2oM3HYyGxq5cWA0rYkQPdkYe7qo38CukWVmcH&type=')
+  //     .then((data) => {
+  //       console.log ('successo');
+  //       console.log(data);
+  //       this.prova = data.data.result[0].album;
+  //     }).catch(() =>{
+  //       console.error('schifo');
+  //     });
+  //   }
+  // }
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
